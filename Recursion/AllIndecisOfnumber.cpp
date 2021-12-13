4039ac5fc90bec0e9bdeee39320d56deb921bdb0
Rishabh Raghav <rishabhraghav2012@gmail.com>
2021-12-13 19:31:48 +0530

    Print all the indices of a number in array using recursion

---
+++ Recursion/AllIndecisOfnumber.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+
+int allIndexes(int input[], int size, int x, int output[]) {
+	if(size == 0) return 0;
+	else {
+        static int i = 0;
+        static int count = 0;
+        
+        if(x == input[0]) {
+            output[count] = i;
+            count++;
+            i++;
+            allIndexes(input + 1, size - 1, x, output);
+        } else {
+            i++;
+            allIndexes(input + 1, size - 1, x, output);
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    int *output = new int[n];
+    
+    int size = allIndexes(input, n, x, output);
+    for(int i = 0; i < size; i++) {
+        cout << output[i] << " ";
+    }
+    
+    delete [] input;
+    
+    delete [] output;
+    
+
+}
+
