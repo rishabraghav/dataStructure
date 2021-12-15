89527b63a6bd3c0d68e98dc743ba8e78ebd22072
Rishabh Raghav <rishabhraghav2012@gmail.com>
2021-12-15 12:22:36 +0530

    removing all the occurences of x in a given string using recursion

---
+++ Recursion/removeX.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+
+
+void removeX(char input[]) {
+    if(input[0] == '\0'){
+        return;
+    }
+   removeX(input + 1);
+    if(input[0] == 'x') {
+        int i = 1;
+        for(; i < strlen(input); i++) {
+            input[i - 1] = input[i];
+        }
+        input[i - 1] = input[i];
+    } 
+     
+	
+}
+
+int main() {
+    char input[100];
+    cin.getline(input, 100);
+    removeX(input);
+    cout << input << endl;
+}
+
