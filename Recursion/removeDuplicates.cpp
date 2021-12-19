c0f8b6eaa57d78e121efd5b9db0ef87a51e5670b
Rishabh Raghav <rishabhraghav2012@gmail.com>
2021-12-19 20:49:24 +0530

    incomplete

---
+++ Recursion/removeDuplicates.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include<cstring>
+
+using namespace std;
+
+void removeConsecutiveDuplicates(char *input) {
+    int size = strlen(input);
+    
+}
+
+int main() {
+    char s[100000];
+    cin >> s;
+    removeConsecutiveDuplicates(s);
+    cout << s << endl;
+}
