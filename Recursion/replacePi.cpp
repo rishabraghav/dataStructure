40503a1a4fde48832a6d71c7c8a727fc7c346b76
Rishabh Raghav <rishabhraghav2012@gmail.com>
2021-12-22 23:22:30 +0530

    incomplete

---
+++ Recursion/replacePi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+
+
+void replacePi(char input[]) {
+	if(strlen(input) == 0 || strlen(input) == 1) {
+        return;
+    }
+    if(input[0] == 'p' && input[1] == 'i') {
+       	for(int i = 0; i < 2; i++) {
+            for(int j = strlen(input) - 1; j >= 0; j--) {
+                input[i + 1] = input[i];
+            }
+        }
+        input[0] = '3';
+        input[1] = '.';
+        input[2] = '1';
+        input[3] = '4';
+        replacePi(input + 4);
+    } else {
+        replacePi(input + 1);
+    }
+}
+
+int main() {
+    char input[10000];
+    cin.getline(input, 10000);
+    replacePi(input);
+    cout << input << endl;
+}
+
